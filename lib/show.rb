a40e58f21abdfd80954fb3132a4dbb100bf5b7b4
Kayden Drake <root@localhost.localdomain>
2019-08-16 14:17:26 -0600

    Adds show initilizer setting and exposing attributes

--- lib/show.rb
+++ lib/show.rb
@@ -1,4 +1,11 @@
 class Show
+  attr_reader :name
+  attr_reader :characters
+  attr_reader :creator
+  
   def initialize(show_name, show_creator, show_characters)
+    @name = show_name
+    @creator = show_creator
+    @characters = show_characters
   end
 end
