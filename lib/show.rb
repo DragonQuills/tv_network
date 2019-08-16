681642deef1fcc583c5c1a7d2abd00e14786e06d
Kayden Drake <root@localhost.localdomain>
2019-08-16 14:28:50 -0600

    Adds calculation of total salary

--- lib/show.rb
+++ lib/show.rb
@@ -2,10 +2,16 @@ class Show
   attr_reader :name
   attr_reader :characters
   attr_reader :creator
-  
+  attr_reader :total_salary
+
   def initialize(show_name, show_creator, show_characters)
     @name = show_name
     @creator = show_creator
     @characters = show_characters
+
+    @total_salary = 0
+    @characters.each do |character|
+      @total_salary += character.salary
+    end
   end
 end
