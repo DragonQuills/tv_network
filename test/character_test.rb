a95c5a5ad78aadf4b7edc511488724624afa61df
Kayden Drake <root@localhost.localdomain>
2019-08-16 13:58:47 -0600

    Adds setup and character test for saving and exposing data

--- test/character_test.rb
+++ test/character_test.rb
@@ -5,8 +5,18 @@ require 'minitest/autorun'
 require './lib/character.rb'
 
 class CharacterTest < Minitest::Test
-  def test_character_exists
-    jd = Character.new({name: "JD", actor: "N/A", salary: "0"})
-    assert_instance_of Character, jd
+  def setup
+    @jd = Character.new({name: "JD", actor: "N/A", salary: 100_000})
   end
+
+  def test_character_exists
+    assert_instance_of Character, @jd
+  end
+
+  def test_character_exposes_data
+    assert_equal "JD", @jd.name
+    assert_equal "N/A", @jd.actor
+    assert_equal 100_000, @jd.salary
+  end
+
 end
