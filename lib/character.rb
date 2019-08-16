f42bb6802525d07187190ebc9b17851320ca9c24
Kayden Drake <root@localhost.localdomain>
2019-08-16 14:02:01 -0600

    Adds character initilization and exposing attributes

--- lib/character.rb
+++ lib/character.rb
@@ -1,4 +1,11 @@
 class Character
+  attr_reader :name
+  attr_reader :actor
+  attr_reader :salary
+
   def initialize(data_hash)
+    @name = data_hash[:name]
+    @actor = data_hash[:actor]
+    @salary = data_hash[:salary]
   end
 end
